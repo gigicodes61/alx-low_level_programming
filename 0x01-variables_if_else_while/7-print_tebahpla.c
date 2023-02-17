e1cba5588e021eb99da031909d51941f9597e71d
Ghizlane <ghizlannouira@gmail.com>
2023-02-17 14:45:36 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabets in reverse
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
