4a6ef7fb4cd8af9ac5bc9cf0eff14c8d5391054d
Ghizlane <ghizlannouira@gmail.com>
2023-02-21 15:07:44 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,14 +2,13 @@
 
 /**
  * jack_bauer - Prints every minute of the day of
- * Jack Bauer, starting from 00:00 to 23:59.
+ *              Jack Bauer, starting from 00:00 to 23:59.
  */
-
 void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour <= 23, hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
